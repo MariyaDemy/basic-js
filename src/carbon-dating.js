08501a9a343f69669020ecef2e9f3f88e269b2ad
Mariya Demy <Mariya.Demi373@gmail.com>
2022-10-15 23:45:33 +0300

    feat: complete dream team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,21 +6,30 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!sampleActivity || isNaN(+sampleActivity)){
+    console.log('false sample')
+    return false;
+  } else {
+    let activity = +sampleActivity;
+    console.log(+sampleActivity, 'activity')
+    console.log((Math.log(15)-Math.log(8))/1.22)
+    //Math.ceil(result)
+  }
 }
+
+dateSample('1')
 
 module.exports = {
   dateSample
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,22 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function createDreamTeam(members) {
+  if(!members || typeof members !== 'object'){
+    return false;
+  } else {
+    let lettersArr = [];
+
+    for(let i=0;i<members.length;i++){
+      if(typeof members[i] == 'string'){
+        let firstLetter = members[i].trim().charAt(0).toUpperCase();
+        lettersArr.push(firstLetter)
+      }
+    }
+
+    return lettersArr.sort().join('');
+  }
 }
+
+//createDreamTeam(['   Matt', 'Ann', 'Dmitry', 'Max'])
 
 module.exports = {
   createDreamTeam

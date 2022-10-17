49402c45d89e4e40c7c6dfeb67b023ae4e987800
Mariya Demy <Mariya.Demi373@gmail.com>
2022-10-17 17:08:13 +0300

    fix: complete carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,28 +1,23 @@
 //const { NotImplementedError } = require('../extensions/index.js');
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
-
 /**
  * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- *
+ * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values *
  * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- *
+ * in case of incorrect sampleActivity *
+ * @example *
  * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
+ * dateSample('WOOT!') => false *
  */
 
  //t = (ln(N0 / N)) / k, where k = 0.693 / HALF_LIFE_PERIOD (5730)
 
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+
 function dateSample(sampleActivity) {
-  if(isNaN(+sampleActivity) || typeof sampleActivity !== 'string'){
-    console.log('false sample')
+  if(isNaN(+sampleActivity) || typeof sampleActivity !== 'string' || +sampleActivity > 15 || +sampleActivity <= 0){
     return false;
   } else {
     let t = (Math.log(MODERN_ACTIVITY / +sampleActivity)) / (0.693 / HALF_LIFE_PERIOD);
@@ -30,7 +25,7 @@ function dateSample(sampleActivity) {
   }
 }
 
-dateSample('1')
+// dateSample('1');
 
 module.exports = {
   dateSample
